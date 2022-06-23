ec7fc1381647f75b1b313356d225d1b68abf6431
Rajput-Suraj <surajarajput@gmail.com>
2022-06-23 12:50:46 +0530

    Implemented functionality to delete using mutation method

--- src/components/PostDetail.jsx
+++ src/components/PostDetail.jsx
@@ -1,4 +1,4 @@
-import { useQuery } from 'react-query';
+import { useQuery, useMutation } from 'react-query';
 
 async function fetchComments(postId) {
   const response = await fetch(`https://jsonplaceholder.typicode.com/comments?postId=${postId}`);
@@ -21,6 +21,8 @@ async function updatePost(postId) {
 }
 
 export function PostDetail({ post }) {
+  const deleteMutation = useMutation((postId) => deletePost(postId));
+
   // replace with useQuery
   const { data, isLoading, isError } = useQuery(['comments', post.id], () =>
     fetchComments(post.id)
@@ -33,7 +35,11 @@ export function PostDetail({ post }) {
   return (
     <div style={{ margin: '20px' }}>
       <h3 style={{ color: 'blue' }}>{post.title}</h3>
-      <button>Delete</button> <button>Update title</button>
+      <button onClick={() => deleteMutation.mutate(post.id)}>Delete</button>
+      {deleteMutation.isError && <h1 style={{ color: 'red' }}>Error deleting post</h1>}
+      {deleteMutation.isLoading && <h1 style={{ color: 'purple' }}>Deleting post</h1>}
+      {deleteMutation.isSuccess && <h1 style={{ color: 'green' }}>Post deleted</h1>}
+      <button>Update title</button>
       <p>{post.body}</p>
       <h4>Comments</h4>
       {data.map((comment) => (
